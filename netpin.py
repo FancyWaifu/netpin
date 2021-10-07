f28707449431b362beb5c2561c24673764d653f2
FancyWaifu <77135613+FancyWaifu@users.noreply.github.com>
2021-10-07 20:20:27 +0000

    Add files via upload

--- netpin.py
+++ netpin.py
@@ -1,15 +1,17 @@
-import keyboard, time
+import keyboard, time, os
 
 print("[*] Beginning pin cracker in 5 seconds")
 time.sleep(5)
 
 for x in range(1111, 10000):
-    print("[*] Trying pin: " + str(x))
-    keyboard.write(str(x)[0])
-    time.sleep(0.1)
-    keyboard.write(str(x)[1])
-    time.sleep(0.1)
-    keyboard.write(str(x)[2])
-    time.sleep(0.1)
-    keyboard.write(str(x)[3])
-    time.sleep(0.1)
+    if keyboard.is_pressed('delete') == 0:
+        print("[*] Trying pin: " + str(x))
+        keyboard.write(str(x)[0])
+        keyboard.write(str(x)[1])
+        keyboard.write(str(x)[2])
+        keyboard.write(str(x)[3])
+        time.sleep(0.001)
+    else:
+        print("[!] Program stopped by user")
+        print("[*] Thanks for using netpin")
+        break
