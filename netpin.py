54c43f38a3840965615a753a67c9321d9d44214d
FancyWaifu <77135613+FancyWaifu@users.noreply.github.com>
2021-10-07 20:38:40 +0000

    Add files via upload

--- netpin.py
+++ netpin.py
@@ -1,4 +1,15 @@
-import keyboard, time, os
+import keyboard, time, os, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--speed", help="speed of the pin cracker, setting to 0 might bug out, based in seconds(Default: 0.5)")
+args = parser.parse_args()
+
+#Pass args to variables
+speed = args.speed
+
+#Check args
+if speed == None:
+    speed = 0.5
 
 print("[*] Beginning pin cracker in 5 seconds")
 time.sleep(5)
@@ -10,7 +21,7 @@ for x in range(1111, 10000):
         keyboard.write(str(x)[1])
         keyboard.write(str(x)[2])
         keyboard.write(str(x)[3])
-        time.sleep(0.001)
+        time.sleep(float(speed))
     else:
         print("[!] Program stopped by user")
         print("[*] Thanks for using netpin")
